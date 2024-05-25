6aa4768f6086466954f0a7bdfcd52c7ffbde4c11
AndreiPaladi <126902857+AndreiPaladi@users.noreply.github.com>
2024-05-25 17:36:55 +0300

    Delete res/player directory

--- res/player/idle/left/cod.ps1
+++
@@ -1,7 +0,0 @@
-$files = Get-ChildItem -Filter *.png
-foreach ($file in $files) {
-    $currentNumber = [int]($file.BaseName -replace '\D')
-    $newNumber = $currentNumber - 1
-    $newFileName = "tile{0:D3}.png" -f $newNumber
-    Rename-Item -Path $file.FullName -NewName $newFileName -Force
-}
